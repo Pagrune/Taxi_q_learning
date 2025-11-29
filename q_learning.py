7ada435e8d38ad2aac77759b8a30aee433668bf1
Pagrune <pauline.grunewald2@gmail.com>
2025-11-29 20:34:48 +0100

    fonctions update_q_table et epsilon_greedy

--- q_learning.py
+++ q_learning.py
@@ -5,42 +5,48 @@ import matplotlib.pyplot as plt
 
 def update_q_table(Q, s, a, r, sprime, alpha, gamma):
     """
-    This function should update the Q function for a given pair of action-state
-    following the q-learning algorithm, it takes as input the Q function, the pair action-state,
-    the reward, the next state sprime, alpha the learning rate and gamma the discount factor.
-    Return the same input Q but updated for the pair s and a.
+    Update Q(s,a) using the Q-learning update rule.
     """
+    max_next_Q = np.max(Q[sprime, :])
+
+    Q[s, a] = Q[s, a] + alpha * (r + gamma * max_next_Q - Q[s, a])
+
+    return Q
 
 
 def epsilon_greedy(Q, s, epsilone):
     """
-    This function implements the epsilon greedy algorithm.
-    Takes as unput the Q function for all states, a state s, and epsilon.
-    It should return the action to take following the epsilon greedy algorithm.
+    Choose an action following epsilon-greedy policy.
     """
+    if np.random.rand() < epsilone:
+        return np.random.randint(Q.shape[1])
+    else:
+        return np.argmax(Q[s, :])
+
 
 
 if __name__ == "__main__":
+    #env = gym.make("Taxi-v3")
     env = gym.make("Taxi-v3", render_mode="human")
+
 
     env.reset()
     env.render()
 
     Q = np.zeros([env.observation_space.n, env.action_space.n])
 
-    alpha = 0.01 # choose your own
+    alpha = 0.3 # choose your own
 
-    gamma = 0.8 # choose your own
+    gamma = 0.95 # choose your own
 
-    epsilon = 0.2 # choose your own
+    epsilon = 0.3 # choose your own
 
-    n_epochs = 20 # choose your own
-    max_itr_per_epoch = 100 # choose your own
+    n_epochs = 2000 # choose your own
+    max_itr_per_epoch = 200 # choose your own
     rewards = []
 
     for e in range(n_epochs):
         r = 0
-
         S, _ = env.reset()
 
         for _ in range(max_itr_per_epoch):
@@ -54,10 +60,11 @@ if __name__ == "__main__":
                 Q=Q, s=S, a=A, r=R, sprime=Sprime, alpha=alpha, gamma=gamma
             )
 
-            # Update state and put a stoping criteria
+            S = Sprime
+            if done:
+                break
 
         print("episode #", e, " : r = ", r)
-
         rewards.append(r)
 
     print("Average reward = ", np.mean(rewards))
